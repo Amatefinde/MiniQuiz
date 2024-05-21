187005c32ab15179a087ed83918592affc4dc3e2
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-05-21 12:52:33 +0300

    redesigned

--- src/components/blocks/ExerciseDifficultyLabel/ExerciseDifficultyLabel.tsx
+++ src/components/blocks/ExerciseDifficultyLabel/ExerciseDifficultyLabel.tsx
@@ -9,13 +9,13 @@ interface IExerciseDifficultyLabelProps {
 
 const ExerciseDifficultyLabel: React.FC<IExerciseDifficultyLabelProps> = ({question}) => {
   if (question.difficulty === "hard") {
-    return <Chip color={"danger"}>Hard</Chip>
+    return <Chip color={"danger"} variant={"outlined"}>Hard</Chip>
   }
   if (question.difficulty === "medium") {
-    return <Chip color={"warning"}>Medium</Chip>
+    return <Chip color={"warning"} variant={"outlined"}>Medium</Chip>
   }
   if (question.difficulty === "easy") {
-    return <Chip color={"success"}>Easy</Chip>
+    return <Chip color={"success"} variant={"outlined"}>Easy</Chip>
   }
   return null;
 };
