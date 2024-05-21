c52b7501a89134966f1cc794feee54dad943d29c
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-05-21 09:04:10 +0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,28 @@ import "@fontsource/inter";
 import StarterPage from "./components/pages/StarterPage/StarterPage.tsx";
 import { CssBaseline, CssVarsProvider } from "@mui/joy";
 import ThemeSwitch from "./components/blocks/ThemeSwitch/ThemeSwitch.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TestingPage from "./components/pages/TestingPage/TestingPage.tsx";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <StarterPage />,
+      errorElement: <h1>Not found 404</h1>,
+    },
+    {
+      path: "/testing",
+      element: (
+          <TestingPage/>
+      ),
+    }])
+
+
   return (
     <CssVarsProvider disableTransitionOnChange>
       <CssBaseline />
-      <StarterPage />
+      <RouterProvider router={router} />
       <ThemeSwitch />
     </CssVarsProvider>
   );
