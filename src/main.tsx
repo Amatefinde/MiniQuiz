e4bc3a81a24b0ed5baff08e06738eb90d7815d1a
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-05-21 10:12:29 +0300

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -7,11 +6,11 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  // <React.StrictMode> // i turn off strict mode because when it rerender component and fetch data second time, backend api block request and return 429 status code :(
     <Provider store={store}>
       <StyledEngineProvider injectFirst>
         <App />
       </StyledEngineProvider>
     </Provider>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 );
