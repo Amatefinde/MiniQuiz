ac3ebac702a46d15e6e8c9926dd3eba277679aca
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-05-21 11:35:23 +0300

    add function for fix markdown

--- src/utils/fetchedQuestionToQuestion.ts
+++ src/utils/fetchedQuestionToQuestion.ts
@@ -2,12 +2,21 @@ import shuffleArray from "./shuffleArray.ts";
 import IFetchedQuestion from "../types/IFetchedQuestion.ts";
 import IQuestion from "../types/IQuestion.ts";
 
+const decodeHtmlEntities = (text: string): string => {
+  const textArea = document.createElement("textarea");
+  textArea.innerHTML = text;
+  return textArea.value;
+};
+
+
 const fetchedQuestionToQuestion = (fetchedQuestion: IFetchedQuestion): IQuestion => {
   return {
     ...fetchedQuestion,
+    question: decodeHtmlEntities(fetchedQuestion.question),
     is_user_answer_correct: undefined,
     user_answer: undefined,
-    answers: shuffleArray([fetchedQuestion.correct_answer, ...fetchedQuestion.incorrect_answers]),
+    answers: shuffleArray([fetchedQuestion.correct_answer, ...fetchedQuestion.incorrect_answers]).map((answer) => decodeHtmlEntities(answer)),
+    correct_answer: decodeHtmlEntities(fetchedQuestion.correct_answer)
   };
 };
 
