d49c9cb7f4e75ef9f23a882b51996e02f26b0205
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-05-21 10:44:08 +0300

    add testing page and base logic

--- src/components/pages/TestingPage/TestingPage.tsx
+++ src/components/pages/TestingPage/TestingPage.tsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuizService from "../../../http/services/QuizService.ts";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../store";
+import { CircularProgress, Typography } from "@mui/joy";
+import axios from "axios";
+import { setQuestions } from "../../../store/questions/questionsSlice.ts";
 
 const TestingPage: React.FC = () => {
+  const quizSettings = useSelector((state: RootState) => state.setting.quizSettings);
+  const questions = useSelector((state: RootState) => state.questions.questions);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [errorStatus, setErrorStatus] = useState<undefined | number>(false);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    async function fetchQuiz() {
+      setIsLoading(true)
+      try {
+        const quizResponse =  await QuizService.getQuiz(quizSettings)
+        dispatch(setQuestions(quizResponse))
+      } catch(err) {
+        if (axios.isAxiosError(err))  {
+          setErrorStatus(err.status)
+        } else {
+          console.log("во время фетча вопросов произошла ошиьбка: ", err)
+        }
+      }
+      setIsLoading(false)
+    }
+
+    fetchQuiz()
+  }, []);
+
+  if (errorStatus) {
+    if (errorStatus === 429) {
+      return <Typography level={"h2"}>Error 429! Too many requests!</Typography>
+    }
+    return <Typography level={"h2"}>Unexpected error</Typography>;
+  }
+
+  if (isLoading) {
+    return <CircularProgress />;
+  }
+
   return (
     <div>
-      привет, тут буду тесты
+      {questions.length > 0 && questions.map(questions => <div>{questions.question}</div>)}
     </div>
   );
 };
