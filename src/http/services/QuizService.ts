0706556f7903862537a15cf17183d7d28079b141
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-05-21 10:43:20 +0300

    change endpoint url

--- src/http/services/QuizService.ts
+++ src/http/services/QuizService.ts
@@ -6,7 +6,7 @@ import IQuizResponse from "../../types/IQuizResponse.ts";
 export default class QuizService {
   static async getQuiz(quizSettings: IQuizSettings): Promise<IQuizResponse> {
     return $api
-      .get("", { params: quizSettings })
+      .get("/", { params: quizSettings })
       .then((response) => response.data);
   }
 }
