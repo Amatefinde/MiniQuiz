080930daf23397600f275db3686ff77084d9a4bf
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-05-21 10:02:34 +0300

    moved to separate component

---
+++ src/components/blocks/ExerciseTypePicker.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Box from '@mui/joy/Box';
+import Radio from '@mui/joy/Radio';
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store";
+import { setQuizSettings } from "../../store/settings/settingsSlice.ts";
+
+const ExerciseTypePicker: React.FC = () => {
+  const quizSettings = useSelector((state: RootState) => state.setting.quizSettings);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(
+      setQuizSettings({
+        ...quizSettings,
+        type: event.target.value,
+      }),
+    );
+  };
+
+  return (
+    <Box sx={{ display: 'flex', gap: 2 }}>
+      <Radio
+        checked={quizSettings.type === 'multiple'}
+        onChange={handleChange}
+        value="multiple"
+        name="radio-buttons"
+        label="Multiple"
+      />
+      <Radio
+        checked={quizSettings.type === 'boolean'}
+        onChange={handleChange}
+        value="boolean"
+        name="radio-buttons"
+        label="Boolean"
+      />
+    </Box>
+  );
+}
+
+export default ExerciseTypePicker;
